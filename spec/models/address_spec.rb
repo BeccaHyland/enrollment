0a6c8fadeaeaa08d0d215f0d6d3ff3f2aa8e6715
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-28 15:45:42 -0600

    adds address model spec validations ATP

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 describe Address, type: :model do
+  describe "Validations" do
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:street_address)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip)}
+  end
+
   describe 'associations' do
     it {should belong_to(:student)}
   end
