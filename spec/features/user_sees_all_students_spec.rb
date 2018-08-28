fdc79b6cd6c5e9db261ba60ca03a9f8b1e5ee6c2
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-28 13:51:48 -0600

    adds student index spec and controller'

---
+++ spec/features/user_sees_all_students_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "user sees all students" do
+  describe "they visit /students" do
+    it "displays all students" do
+      student1 = Student.create!(name: "Becca")
+      student2 = Student.create!(name: "Tim")
+
+      visit students_path
+
+      expect(page).to have_content(student1.name)
+      expect(page).to have_content(student2.name)
+    end
+  end
+end
