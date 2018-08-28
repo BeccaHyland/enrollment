485ce6494e9597e7df95bad3395a8a28e200033e
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-28 14:23:24 -0600

    adds student show functionality gs

--- spec/features/students/user_sees_one_student_spec.rb
+++ spec/features/students/user_sees_one_student_spec.rb
@@ -1,1 +1,17 @@
 require 'rails_helper'
+
+describe "user sees one student" do
+	describe "they link from the students index" do
+		it "displays information for one student" do
+      student1 = Student.create!(name: "Becca")
+      student2 = Student.create!(name: "Tim")
+
+      visit students_path
+
+      click_link student1.name
+
+      expect(current_path).to eq(student_path(student1))
+      expect(page).to have_content(student1.name)
+		end
+	end
+end
